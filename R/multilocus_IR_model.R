7612d83ed201870615dfce5d06ac3d5c94368c2c
MiharisoaSylviane <miharisoasylviane@gmail.com>
2025-09-15 16:30:30 +0300

    to compute the fraction of each multilocus genotype. Our code is working at locus and genotype level, but we will modify after to add times scale

--- R/multilocus_IR_model.R
+++ R/multilocus_IR_model.R
@@ -5,23 +5,34 @@ p <- runif(0,1)
 n_loci <- 2
 # call the dummy function
 source("C:/Users/Sylviane/Desktop/training_perth_2024/phd_ir_mapping/R/create_dummy_matrices.R")
-genotype_combination <- create_dummy_matrices()
+genotype_combination <- create_dummy_matrices(n_loci=2)
+
+L <- genotype_combination$left
+L
+R <- genotype_combination$right
+L[3, 2]
 ##multilocus genotype frequencies
 # we will use the dummy matrice of the left and right hand of the allele, 
 # we define a function F which is the genotype probability across the n_loci
 # sweep(x, MARGIN, STATS, FUN) : it allows Sylviane to operate value in matrix,
 # x= matrix name, margin = to perform on Row = 1, on column =2, stats = the value
-# to use (formula), function is the * or / or +
+# to use (formula), function is the * or / or +, 
 # 
- genotype_probabilty <- function(p) {
-  #combination of the alleles
-   comb_gen <- genotype_combination(n_loci)
-  # A= (1-p)*L + p*(1-L)
-  left_prob  <- sweep(left,  2, 1 - p, "*") + sweep(1 - left,  2, p, "*")
-  # B= (1-p)*R + p*(1-R)
-  right_prob <- sweep(right, 2, 1 - p, "*") + sweep(1 - right, 2, p, "*")
-  # A*B
-  F <- left_prob * right_prob
-  return(F)
-}
-genotype_probabilty(n_loci = 2, p)
+ probability_genotype <- function(p){
+   G <- nrow(L)
+   B <- ncol(L)
+   if(length(p) != B){stop("locus probability dimension mismatch")}
+   F <- matrix(data=NA, nrow = G, ncol= B)
+   for (g in seq_len(G)) {
+     for (lo in seq_len(B)) {
+    prob_left <- (1-p[lo]) * L[g,lo] + p[lo]* (1-L[g,lo])
+    prob_right <- (1-p[lo]) * R[g,lo] + p[lo]* (1-R[g,lo])
+     F[g,lo] <- prob_left * prob_right
+     }
+   }
+   return(F) 
+ }
+ 
+
+
+ 
