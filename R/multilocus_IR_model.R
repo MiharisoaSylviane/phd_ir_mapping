840285e30d09720b6c0794efc25663099727e553
MiharisoaSylviane <miharisoasylviane@gmail.com>
2025-09-18 12:10:20 +0300

    to add a function that contain the w, gw, z at time point t, and z at next time point

--- R/multilocus_IR_model.R
+++ R/multilocus_IR_model.R
@@ -1,7 +1,7 @@
 set.seed(123)
 ## Prior
 # p is the initial allele frequency
-p <- runif(0,1)
+
 n_loci <- 2
 # call the dummy function
 source("C:/Users/Sylviane/Desktop/training_perth_2024/phd_ir_mapping/R/create_dummy_matrices.R")
@@ -38,9 +38,11 @@ probability_genotype <- function(p){
     #   z[g] <- z[g] * F[g, lo]
     # }
   }
-  return(z) 
+  z[g] <- prod(F[g, ])
 }
- 
+p <- runif(B)
+
+### fast way to get genotype perlocus
 probability_genotype_fast <- function(p){
   G <- nrow(L)
   B <- ncol(L)
@@ -61,176 +63,116 @@ probability_genotype_fast <- function(p){
   return(z) 
 }
 
+
+# p : allele frequency at each locus 
+p <- runif(B) 
+# genotype frequency as p is a matrix and returning a vector z
+probability_genotype_fast <- function(p){
+  stopifnot(is.matrix(L), is.matrix(R), all(dim(L) == dim(R)))
+  G <- nrow(L); B <- ncol(L)
+  stopifnot(length(p) == B)
+  
+  prob_left  <- sweep(L, 2, 1 - p, "*") + sweep(1 - L, 2, p, "*")
+  prob_right <- sweep(R, 2, 1 - p, "*") + sweep(1 - R, 2, p, "*")
+  dup <- 1 + L - R   # =2 pour (1,0), =1 sinon
+  
+  F <- prob_left * prob_right * dup
+  z_unnorm <- apply(F, 1, prod)
+  z <- z_unnorm / sum(z_unnorm)  # vecteur longueur G
+  z
+}
+# to test our function
+test <- probability_genotype_fast(p)
+test
+
+## selection at locus level
 ## the effect of intervention 
 K <- 4
- # Xn <- c(0.1, 0.2,0.3,0.4)
+probability_genotype_fast <- function(p){
+  G <- nrow(L); B <- ncol(L)
+  if (length(p) != B) stop("locus probability dimension mismatch")
+  
+  # proba d'observer l'allèle encodé en L et R (colonne par colonne)
+  prob_left  <- sweep(L, 2, 1 - p, "*") + sweep(1 - L, 2, p, "*")  # L==1 -> 1-p ; L==0 -> p
+  prob_right <- sweep(R, 2, 1 - p, "*") + sweep(1 - R, 2, p, "*")  # R==1 -> 1-p ; R==0 -> p
+  
+  # facteur de doublon (2 pour hétérozygote gardé (1,0), 1 sinon)
+  dup <- 1 + L - R
+  
+  # probabilité par locus, puis produit sur les loci -> masse par génotype (ligne)
+  F <- prob_left * prob_right * dup
+  z_unnorm <- apply(F, 1, prod)     # <- longueur G (une par combinaison L/R)
+  
+  # normalisation -> vecteur de proba
+  s <- sum(z_unnorm)
+  if (!is.finite(s) || s == 0) stop("Somme nulle/non-finie pour z; vérifiez L/R/p")
+  z <- z_unnorm / s
+  return(z)                          # <-- VECTEUR longueur G
+}
+
+# Xn <- c(0.1, 0.2,0.3,0.4)
 X <- runif(K)
 betamat <- matrix(rnorm(B * K), nrow=B, ncol=K)
- gammax <- exp(betamat)
- s <- rep(0, B)
- w <- rep(NA, B)
- for(lo in seq_len(B)) {
-   for (k in seq_len(K)) {
-     s[lo] <- s[lo] + X[k] * gammax[lo, k]
-   }
-   w[lo] <- 1 + s[lo]
- }
- 
- # # or
- # for(lo in seq_len(B)) {
- #   s[lo] <- sum(X * gammax[lo, ])
- # }
- # 
- # # or
- # s <- rowSums(sweep(gammax, 2, X, FUN = "*"))
- # # or
- # s <- t(gammax %*% X)
- # # or
- # s <- X %*% t(gammax)
- 
- # selection pressure
- # st <- gammax*itns_covariates
- # # relative fitness of mosquitoes with allele resistant R/S
- # w <- 1 + st
-h <- runif(B, 0,1)
-time_series <- 10
+gammax <- exp(betamat)
+s <- rep(0, B)
+w <- rep(NA, B)
+for(lo in seq_len(B)) {
+  for (k in seq_len(K)) {
+    s[lo] <- s[lo] + X[k] * gammax[lo, k]
+  }
+  w[lo] <- 1 + s[lo]
+}
+
+# # or
+# for(lo in seq_len(B)) {
+#   s[lo] <- sum(X * gammax[lo, ])
+# }
+# 
+# # or
+# s <- rowSums(sweep(gammax, 2, X, FUN = "*"))
+# # or
+# s <- t(gammax %*% X)
+# # or
+# s <- X %*% t(gammax)
+
+# selection pressure
+# st <- gammax*itns_covariates
+# # relative fitness of mosquitoes with allele resistant R/S
+# w <- 1 + st
+
+
+
+## GENOTYPE NEXT STEP
+# step to get the genotype frequency at next step without using time series
 polygenic_multilocus_next_step <- function(w, h){
-   G <- nrow(L)
-   B <- ncol(L)
-   Gw <- matrix(data=NA, nrow = G, ncol= B)
-   if(length(w) != B){stop("locus probability dimension mismatch")}
-   for (t in 1:(time_series)){
-   for (g in seq_len(G)) {
-     for (lo in seq_len(B)) {
-       # polygenic fitness
-       Gw[g, lo, t] <- 1 * R[g, lo, t] +
-         w[lo, t] * (1 - L[g, lo, t]) +
-         (w[lo,t] * h[lo,t] + 1 - h[lo,t]) * (L[g, lo,t] - R[g, lo,t])
-         heterozygote_duplicates <- 1 + L - R
-      r <- apply(Gw, 1, prod) 
-     }
-     unpost [g,t+1] <- z[g,t] * r[g,t]
-   }
-     post[g,t+1] / sum(post[g,t+1])
-   }
+  stopifnot(is.matrix(L), is.matrix(R), all(dim(L) == dim(R)))
+ # define G and B 
+  G <- nrow(L); B <- ncol(L)
+  stopifnot(length(w) == B, length(h) == B)
+  stopifnot(exists("z"), length(z) == G)
+ # define an empty matrix to put the value of the fitness polygenic in depend of 
+  # the zygosity (homo - hetero) of our hand of alleles
+  Gw <- matrix(NA_real_, nrow = G, ncol = B)
+  # define the combination SS and RR and RS to give more sense to the calculation
+  for (lo in seq_len(B)) {
+    SS  <- (L[, lo] == 1L) & (R[, lo] == 1L)      # (1,1)
+    RR  <- (L[, lo] == 0L) & (R[, lo] == 0L)      # (0,0)
+    SR  <- (L[, lo] == 1L) & (R[, lo] == 0L)      # (1,0)
+    
+    Gw[, lo] <- 1 * SS +
+      w[lo] * RR +
+      (h[lo] * w[lo] + (1 - h[lo])) * SR
+  }
+  
+  r_vec  <- apply(Gw, 1, prod)
+  unpost <- z * r_vec
+  post   <- unpost / sum(unpost)
+  
+  list(Gw = Gw,
+       r = matrix(r_vec,  nrow = G, ncol = 1),
+       post_next = matrix(post, nrow = G, ncol = 1),
+       z = matrix(z,     nrow = G, ncol = 1))
 }
-
-
-
-# assumes globals: L, R, z
-
-
-
-
-
-
-
-
-
-
-
-
-
-# multilocus genotype at next step with all parameters
-p <- rbeta(B, 2, 2)         # prior per locus (safer than runif)
-
-polygenic_multilocus_next_step<- function( w, h) {
-   method <- match.arg(method)
-   #checking for each matrix 
-   stopifnot(is.matrix(L), 
-             is.matrix(R), 
-             all(dim(L) == dim(R)))
-             G <- nrow(L)
-             B <- ncol(L)
-             stopifnot(length(w) == B, length(h) == B)
-             if (is.null(dim(w))) w <- matrix(rep(w, T), nrow = B, ncol = T)
-             if (is.null(dim(h))) h <- matrix(rep(h, T), nrow = B, ncol = T)
-             Gw     <- array(NA_real_, dim = c(G, B, T))
-             r      <- matrix(NA_real_, nrow = G, ncol = T)
-             unpost <- matrix(NA_real_, nrow = G, ncol = T + 1)
-             post   <- matrix(NA_real_, nrow = G, ncol = T + 1)
-   # assign a matrix to 
-   p <- matrix(p, G, B, byrow = TRUE)
-   W <- matrix(w, G, B, byrow = TRUE)
-   H <- matrix(h, G, B, byrow = TRUE)
-   
-   # genotype
-   prob_left  <- L * (1 - p) + (1 - L) * p
-   prob_right <- R * (1 - p) + (1 - R) * p
-   z <- apply(prob_left * prob_right * (1 + L - R), 1, prod)
-   
-   # polygenic fitness
-   Gw <- R + W * (1 - L) + (W * H + 1 - H) * (L - R)
-   r <- apply(Gw, 1, prod)
-   
-   # posterior over genotypes
-   unpost <- z * r
-   post <- unpost/ sum(unpost)
- }
-
-post <- posterior_genotype(w, h)
- 
-### Allele frequency at next step a(t+1) in depend of z
-
-
-
- 
-
-
-
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- ## this is for gneotype and locus and times scale
-time
-probability_genotype <- function(p){
-  G <- nrow(L)
-  B <- ncol(L)
-  p <- matrix(p, nrow = B, ncol = time_series) 
-  Tn <- ncol(p)
-  if(length(p) != B){stop("locus probability dimension mismatch")}
-  F <- matrix(data=NA, dim = c(G, B, Tn)) #F <- matrix(data=NA, nrow = G, ncol= B)
-  for (t in seq_len(Tn))
-    for (g in seq_len(G)) {
-      for (lo in seq_len(B)) {
-        prob_left <- (1-p[lo,t]) * L[g,lo,t] + p[lo,t]* (1-L[g,lo,t])
-        prob_right <- (1-p[lo,t]) * R[g,lo,t] + p[lo,t]* (1-R[g,lo,t])
-        F[g,lo,t] <- prob_left * prob_right
-      }
-    }
-  return(F) 
-}
-
-
 
 
 
