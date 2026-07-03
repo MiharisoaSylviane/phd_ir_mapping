9a7d4e11ce01b12af1a3bffa4dd5b30f34885d13
MiharisoaSylviane <miharisoasylviane@gmail.com>
2026-07-03 13:41:33 +0300

    to remove the sum(theta) and fix the function to estimate the phenotype

--- R/function_geno_pheno.R
+++ R/function_geno_pheno.R
@@ -18,6 +18,7 @@ compute_w_greta <- function(betamat, X) {
 probability_genotype_fast_greta <- function(p, L, R) {
   prob_left  <- sweep(L, 2, 1 - p, "*") + sweep(1 - L, 2, p, "*")
   prob_right <- sweep(R, 2, 1 - p, "*") + sweep(1 - R, 2, p, "*")
+  dup <- 1 + L - R
   z <- apply(prob_left * prob_right * dup, 1, "prod")   
   z / sum(z)
 }
@@ -100,51 +101,104 @@ simulate_genotype_timecourse_greta <- function(p, w, h, Tmax,
 }
 
 # Phenotype function
-compute_Ugc <- function(L, R, w, h) {
-  G <- nrow(L)
-  B <- ncol(L)
-  f <- matrix(NA, nrow = G, ncol = B)
-  
-  for (l in 1:B) {
-    SS <- (L[, l] == 1 & R[, l] == 1)
-    RR <- (L[, l] == 0 & R[, l] == 0)
-    SR <- (L[, l] == 1 & R[, l] == 0) | (L[, l] == 0 & R[, l] == 1)
-    
-    f[, l] <- 1 * SS +
-      w[l] * RR +
-      (h[l] * w[l] + (1 - h[l])) * SR
-  }
-  
-  return(f)
+# compute_Ugc <- function(L, R, w, h) {
+#   G <- nrow(L)
+#   B <- ncol(L)
+#   f <- matrix(NA, nrow = G, ncol = B)
+#   
+#   for (l in 1:B) {
+#     SS <- (L[, l] == 1 & R[, l] == 1)
+#     RR <- (L[, l] == 0 & R[, l] == 0)
+#     SR <- (L[, l] == 1 & R[, l] == 0) | (L[, l] == 0 & R[, l] == 1)
+#     
+#     f[, l] <- 1 * SS +
+#       w[l] * RR +
+#       (h[l] * w[l] + (1 - h[l])) * SR
+#   }
+#   
+#   return(f)
+# }
+### PHENOTYPE FUNCTION
+compute_Ugc <- function(theta, h, SS_mask, RR_mask, SR_mask) {
+  SS_mask +
+    sweep(RR_mask, 2, theta, "*") +
+    sweep(SR_mask, 2, h * theta + (1 - h), "*")
 }
 
-compute_Ustar <- function(Ugc, theta, type = "additive", epsilon = NULL) {
+# compute_Ustar <- function(Ugc, theta, type = "", epsilon = NULL) {
+#   
+#   U_add <- rowSums(Ugc)
+#   U_mult <- apply(Ugc, 1, prod)
+#   
+#   if (type == "additive") {
+#     U_star <- U_add
+#     
+#   } else if (type == "multiplicative") {
+#     U_star <- U_mult
+#     
+#   } else if (type == "epistatic") {
+#     if (is.null(epsilon)) stop("epsilon required")
+#     pairwise_epi <- rowSums(Ugc %*% epsilon * Ugc)
+#     U_star <- U_add + U_mult + pairwise_epi
+#   }
+#   
+#   U_star <- U_star / sum(theta)
+#   return(U_star)
+# }
+
+# the function following is allowing us to estimate the pehenotype frequency
+# from the genotype
+
+# multiplicative (indepedent barriers) is goverm by the assumptions:
+
+## - that genotype of differents locus are acting as "filter survival"
+## that means that if we have two locus: one metabolic and one target site
+## the insecticide will be metabolised by the enzyme metabolic then pass
+## and will arrive in the target site gene but this gene already modified
+##  the proteins
+compute_Ustar_multiplicative<- function(Ugc, theta, epsilon = NULL) {
   
-  U_add <- rowSums(Ugc)
-  U_mult <- apply(Ugc, 1, prod)
-  
-  if (type == "additive") {
-    U_star <- U_add
-    
-  } else if (type == "multiplicative") {
-    U_star <- U_mult
-    
-  } else if (type == "epistatic") {
-    if (is.null(epsilon)) stop("epsilon required")
-    pairwise_epi <- rowSums(Ugc %*% epsilon * Ugc)
-    U_star <- U_add + U_mult + pairwise_epi
-  }
-  
-  U_star <- U_star / sum(theta)
+  #U_mult <- apply(Ugc, 1, prod)
+  U_mult <- exp(rowSums(log(Ugc + 1e-12)))
+
+  U_star <- U_mult # normailizing this with sum(theta was a bad idea)
   return(U_star)
 }
 
+# additive (contribution for the protection)
+# assumptions is govern by the assumptions:
+
+## - that the genotype of differents locus will contribute a small on the resistant
+## so here it's not like checking anymore but each resistance will
+## add some protection to the mosquitoes so acting on the survival
+compute_Ustar_additive <- function(Ugc, theta, epsilon = NULL) {
+  
+  U_add <- rowSums(Ugc)
+  
+  U_star <- U_add / ncol(Ugc) # here I some it by the total of Ugc instead of theta
+  # this should be discussed with Nick
+  return(U_star)
+}
+
+# compute_p_died <- function(U_star) {
+#   p <- 1 - U_star
+#   p[p < 0] <- 0
+#   p[p > 1] <- 1
+#   return(p)
+# } # the version is hated by greta 
+
+# compute_p_died <- function(U_star) {
+#   p <- U_star # 1- ustar give us the survival of resistant
+#   p <- p * (p > 0)              # zero out anything below 0
+#   p <- p * (p <= 1) + (p > 1)   # cap anything above 1 at exactly 1
+#   return(p)
+# }
 compute_p_died <- function(U_star) {
-  p <- 1 - U_star
-  p[p < 0] <- 0
-  p[p > 1] <- 1
+  eps <- 1e-6
+  p <- eps + (1 - 2*eps) * U_star   # linearly rescales [0,1] into (eps, 1-eps)
   return(p)
 }
+# this is to model the phenotype based on his likelihood here
 
 simulate_beta_binomial <- function(p, n, phi = 20) {
   alpha <- p * phi
@@ -154,23 +208,25 @@ simulate_beta_binomial <- function(p, n, phi = 20) {
   return(list(p_sample = p_sample, y = y))
 }
 
-pheno <- function(Z_mat, effect_type = "additive", alpha = NULL, epsilon = NULL) {
-  p <- ncol(Z_mat)
-  
-  if (is.null(alpha)) alpha <- runif(p, 0.1, 1)
-  
-  if (effect_type == "additive") {
-    pheno <- as.vector(as.matrix(Z_mat) %*% alpha)
-    
-  } else if (effect_type == "epistatic") {
-    if (is.null(epsilon)) epsilon <- matrix(runif(p^2, -0.2, 0.2), p, p)
-    pheno <- as.vector(as.matrix(Z_mat) %*% alpha +
-                         rowSums((Z_mat %*% epsilon) * Z_mat))
-  }
-  
-  pheno <- (pheno - min(pheno)) / (max(pheno) - min(pheno))
-  return(pheno)
-}
+# pheno <- function(Z_mat, effect_type = "additive", alpha = NULL, epsilon = NULL) {
+#   p <- ncol(Z_mat)
+#   
+#   if (is.null(alpha)) alpha <- runif(p, 0.1, 1)
+#   
+#   if (effect_type == "additive") {
+#     pheno <- as.vector(as.matrix(Z_mat) %*% alpha)
+#     
+#   } else if (effect_type == "epistatic") {
+#     if (is.null(epsilon)) epsilon <- matrix(runif(p^2, -0.2, 0.2), p, p)
+#     pheno <- as.vector(as.matrix(Z_mat) %*% alpha +
+#                          rowSums((Z_mat %*% epsilon) * Z_mat))
+#   }
+#   
+#   pheno <- (pheno - min(pheno)) / (max(pheno) - min(pheno))
+#   return(pheno)
+# }
+
+
 
 # Allele frequency from genotype
 
